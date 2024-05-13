3725e43f12392a258a3ebe8b004d7bd03a1cf423
leejun <hubo.move@gmail.com>
2024-05-13 15:51:44 +0900

    init

---
+++ pyBNO055.py
@@ -0,0 +1,201 @@
+
+BNO055_ADDR                     = 0x28
+
+BNO055_PAGE_ID_ADDR             = 0x07
+# PAGE0 REGISTER DEFINITION
+BNO055_CHIP_ID_ADDR             = 0x00
+BNO055_ACC_ID_ADDR              = 0x01
+BNO055_MAG_ID_ADDR              = 0x02
+BNO055_GYR_ID_ADDR              = 0x03
+BNO055_SW_REV_ID_LSB_ADDR       = 0x04
+BNO055_SW_REV_ID_MSB_ADDR       = 0x05
+BNO055_BL_REV_ID_ADDR           = 0x06
+BNO055_ACC_DATA_X_LSB_ADDR      = 0x08
+BNO055_ACC_DATA_X_MSB_ADDR      = 0x09
+BNO055_ACC_DATA_Y_LSB_ADDR      = 0x0A
+BNO055_ACC_DATA_Y_MSB_ADDR      = 0x0B
+BNO055_ACC_DATA_Z_LSB_ADDR      = 0x0C
+BNO055_ACC_DATA_Z_MSB_ADDR      = 0x0D
+BNO055_MAG_DATA_X_LSB_ADDR      = 0x0E
+BNO055_MAG_DATA_X_MSB_ADDR      = 0x0F
+BNO055_MAG_DATA_Y_LSB_ADDR      = 0x10
+BNO055_MAG_DATA_Y_MSB_ADDR      = 0x11
+BNO055_MAG_DATA_Z_LSB_ADDR      = 0x12
+BNO055_MAG_DATA_Z_MSB_ADDR      = 0x13
+BNO055_GYR_DATA_X_LSB_ADDR      = 0x14
+BNO055_GYR_DATA_X_MSB_ADDR      = 0x15
+BNO055_GYR_DATA_Y_LSB_ADDR      = 0x16
+BNO055_GYR_DATA_Y_MSB_ADDR      = 0x17
+BNO055_GYR_DATA_Z_LSB_ADDR      = 0x18
+BNO055_GYR_DATA_Z_MSB_ADDR      = 0x19
+BNO055_EUL_HEADING_LSB_ADDR     = 0x1A
+BNO055_EUL_HEADING_MSB_ADDR     = 0x1B
+BNO055_EUL_ROLL_LSB_ADDR        = 0x1C
+BNO055_EUL_ROLL_MSB_ADDR        = 0x1D
+BNO055_EUL_PITCH_LSB_ADDR       = 0x1E
+BNO055_EUL_PITCH_MSB_ADDR       = 0x1F
+BNO055_QUA_DATA_W_LSB_ADDR      = 0x20
+BNO055_QUA_DATA_W_MSB_ADDR      = 0x21
+BNO055_QUA_DATA_X_LSB_ADDR      = 0x22
+BNO055_QUA_DATA_X_MSB_ADDR      = 0x23
+BNO055_QUA_DATA_Y_LSB_ADDR      = 0x24
+BNO055_QUA_DATA_Y_MSB_ADDR      = 0x25
+BNO055_QUA_DATA_Z_LSB_ADDR      = 0x26
+BNO055_QUA_DATA_Z_MSB_ADDR      = 0x27
+BNO055_LIA_DATA_X_LSB_ADDR      = 0x28
+BNO055_LIA_DATA_X_MSB_ADDR      = 0x29
+BNO055_LIA_DATA_Y_LSB_ADDR      = 0x2A
+BNO055_LIA_DATA_Y_MSB_ADDR      = 0x2B
+BNO055_LIA_DATA_Z_LSB_ADDR      = 0x2C
+BNO055_LIA_DATA_Z_MSB_ADDR      = 0x2D
+BNO055_GRAV_DATA_X_LSB_ADDR     = 0x2E
+BNO055_GRAV_DATA_X_MSB_ADDR     = 0x2F
+BNO055_GRAV_DATA_Y_LSB_ADDR     = 0x30
+BNO055_GRAV_DATA_Y_MSB_ADDR     = 0x31
+BNO055_GRAV_DATA_Z_LSB_ADDR     = 0x32
+BNO055_GRAV_DATA_Z_MSB_ADDR     = 0x33
+BNO055_TEMP_ADDR                = 0x34
+BNO055_CALIB_STAT_ADDR          = 0x35
+BNO055_ST_RESULT_ADDR           = 0x36
+BNO055_INT_STA_ADDR             = 0x37
+BNO055_SYS_CLK_STAT_ADDR        = 0x38
+BNO055_SYS_STATUS_ADDR          = 0x39
+BNO055_SYS_ERR_ADDR             = 0x3A
+BNO055_UNIT_SEL_ADDR            = 0x3B
+BNO055_OPR_MODE_ADDR            = 0x3D
+BNO055_PWR_MODE_ADDR            = 0x3E
+BNO055_SYS_TRIGGER_ADDR         = 0x3F
+BNO055_TEMP_SOURCE_ADDR         = 0x40
+BNO055_AXIS_MAP_CONFIG_ADDR     = 0x41
+BNO055_AXIS_MAP_SIGN_ADDR       = 0x42
+BNO055_ACC_OFFSET_X_LSB_ADDR    = 0x55
+BNO055_ACC_OFFSET_X_MSB_ADDR    = 0x56
+BNO055_ACC_OFFSET_Y_LSB_ADDR    = 0x57
+BNO055_ACC_OFFSET_Y_MSB_ADDR    = 0x58
+BNO055_ACC_OFFSET_Z_LSB_ADDR    = 0x59
+BNO055_ACC_OFFSET_Z_MSB_ADDR    = 0x5A
+BNO055_MAG_OFFSET_X_LSB_ADDR    = 0x5B
+BNO055_MAG_OFFSET_X_MSB_ADDR    = 0x5C
+BNO055_MAG_OFFSET_Y_LSB_ADDR    = 0x5D
+BNO055_MAG_OFFSET_Y_MSB_ADDR    = 0x5E
+BNO055_MAG_OFFSET_Z_LSB_ADDR    = 0x5F
+BNO055_MAG_OFFSET_Z_MSB_ADDR    = 0x60
+BNO055_GYR_OFFSET_X_LSB_ADDR    = 0x61
+BNO055_GYR_OFFSET_X_MSB_ADDR    = 0x62
+BNO055_GYR_OFFSET_Y_LSB_ADDR    = 0x63
+BNO055_GYR_OFFSET_Y_MSB_ADDR    = 0x64
+BNO055_GYR_OFFSET_Z_LSB_ADDR    = 0x65
+BNO055_GYR_OFFSET_Z_MSB_ADDR    = 0x66
+BNO055_ACC_RADIUS_LSB_ADDR      = 0x67
+BNO055_ACC_RADIUS_MSB_ADDR      = 0x68
+BNO055_MAG_RADIUS_LSB_ADDR      = 0x69
+BNO055_MAG_RADIUS_MSB_ADDR      = 0x6A
+# PAGE0 REGISTER DEFINITION
+
+# PAGE1 REGISTER DEFINITION
+BNO055_ACC_CONFIG_ADDR          = 0x08
+BNO055_MAG_CONFIG_ADDR          = 0x09
+BNO055_GYR_CONFIG_0_ADDR        = 0x0A
+BNO055_GYR_CONFIG_1_ADDR        = 0x0B
+BNO055_ACC_SLEEP_CONFIG_ADDR    = 0x0C
+BNO055_GYR_SLEEP_CONFIG_ADDR    = 0x0D
+BNO055_INT_MSK_ADDR             = 0x0F
+BNO055_INT_EN_ADDR              = 0x10
+BNO055_ACC_AM_THRES_ADDR        = 0x11
+BNO055_ACC_INT_SETTINGS_ADDR    = 0x12
+BNO055_ACC_HG_DURATION_ADDR     = 0x13
+BNO055_ACC_HG_THRES_ADDR        = 0x14
+BNO055_ACC_NM_THRES_ADDR        = 0x15
+BNO055_ACC_NM_SET_ADDR          = 0x16
+BNO055_GYR_INT_SETTING_ADDR     = 0x17
+BNO055_GYR_HR_X_SET_ADDR        = 0x18
+BNO055_GYR_DUR_X_ADDR           = 0x19
+BNO055_GYR_HR_Y_SET_ADDR        = 0x1A
+BNO055_GYR_DUR_Y_ADDR           = 0x1B
+BNO055_GYR_HR_Z_SET_ADDR        = 0x1C
+BNO055_GYR_DUR_Z_ADDR           = 0x1D
+BNO055_GYR_AM_THRESH_ADDR       = 0x1E
+BNO055_GYR_AM_SET_ADDR          = 0x1F
+# PAGE1 REGISTER DEFINITION
+
+# BNO055 operation mode settings
+BNO055_OPR_MODE_CONFIGMODE      = 0x00
+## non-fusion mode
+BNO055_OPR_MODE_ACCONLY         = 0x01
+BNO055_OPR_MODE_MAGONLY         = 0x02
+BNO055_OPR_MODE_GYRONLY         = 0x03
+BNO055_OPR_MODE_ACCMAG          = 0x04
+BNO055_OPR_MODE_ACCGYRO         = 0x05
+BNO055_OPR_MODE_MAGGYRO         = 0x06
+## fusion mode
+BNO055_OPR_MODE_AMG             = 0x07
+BNO055_OPR_MODE_IMU             = 0x08
+BNO055_OPR_MODE_COMPASS         = 0x09
+BNO055_OPR_MODE_M4G             = 0x0A
+BNO055_OPR_MODE_NDOF_FMC_OFF    = 0x0B
+BNO055_OPR_MODE_NDOF            = 0x0C
+
+from smbus2 import SMBus
+import time
+import struct
+
+class BNO055:
+    __buf = []
+
+    def __init__(self):
+        self.i2c = SMBus(1)
+
+    def readChipID(self):
+        self.i2c.write_byte(BNO055_ADDR, BNO055_CHIP_ID_ADDR)
+        return self.i2c.read_byte(BNO055_ADDR)
+    
+    def readAccID(self):
+        self.i2c.write_byte(BNO055_ADDR, BNO055_ACC_ID_ADDR)
+        return self.i2c.read_byte(BNO055_ADDR)
+    
+    def readMagID(self):
+        self.i2c.write_byte(BNO055_ADDR, BNO055_MAG_ID_ADDR)
+        return self.i2c.read_byte(BNO055_ADDR)
+    
+    def setMode(self, mode):
+        self.i2c.write_byte_data(BNO055_ADDR, BNO055_OPR_MODE_ADDR, mode)
+        if mode == BNO055_OPR_MODE_CONFIGMODE:
+            time.sleep(0.012)
+        else:
+            time.sleep(0.007)
+        
+    def setUnit(self, acc, gyro, euler, temp):
+        self.i2c.write_byte_data(BNO055_ADDR, BNO055_UNIT_SEL_ADDR, (acc) | (gyro << 1) | (euler << 2) | (temp << 4))
+
+    def getQuaternion(self):
+        block = self.i2c.read_i2c_block_data(BNO055_ADDR, BNO055_QUA_DATA_W_LSB_ADDR, 8)
+        unpacked = struct.unpack("hhhh", bytes(block))
+        qw = unpacked[0] / (1 << 14)
+        qx = unpacked[1] / (1 << 14)
+        qy = unpacked[2] / (1 << 14)
+        qz = unpacked[3] / (1 << 14)
+        return qw, qx, qy, qz
+
+    def getGyro(self):
+        block = self.i2c.read_i2c_block_data(BNO055_ADDR, BNO055_GYR_DATA_X_LSB_ADDR, 6)
+        unpacked = struct.unpack("hhh", bytes(block))
+        gx = unpacked[0] / 900.0
+        gy = unpacked[1] / 900.0
+        gz = unpacked[2] / 900.0
+        return gx, gy, gz
+
+if __name__ == "__main__":
+    bno055 = BNO055()
+    print("Chip ID: 0x{:02X}".format(bno055.readChipID()))
+    print("Acc ID: 0x{:02X}".format(bno055.readAccID()))
+    print("Mag ID: 0x{:02X}".format(bno055.readMagID()))
+    bno055.setMode(BNO055_OPR_MODE_IMU)
+    bno055.setUnit(0, 1, 1, 0)
+    time.sleep(2)
+    while True:
+        qw, qx, qy, qz = bno055.getQuaternion()
+        gx, gy, gz = bno055.getGyro()
+        print("Quaternion: ({:.2f}, {:.2f}, {:.2f}, {:.2f})".format(qw, qx, qy, qz))
+        print("Gyro: ({:.2f}, {:.2f}, {:.2f})".format(gx, gy, gz))
+        time.sleep(0.1)
+s
